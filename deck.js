befeefd26bb4a9835224b4ea1d81bb71449a6583
Kevin Klein <pkevinklein88@gmail.com>
2021-01-24 19:56:44 +0100

    Initial commit

---
+++ deck.js
@@ -0,0 +1,31 @@
+const values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const suits = ["♠", "♣", "♥", "♦"];
+
+export default class Deck {
+  constructor(cards = newDeck()) {
+    this.cards = cards;
+    this.numberOfCards = this.cards.length;
+    this.discardPile = [];
+  }
+  shuffleCards(cards) {
+    for (let i = cards.length - 1; i > 0; i--) {
+      const newIndex = Math.floor(Math.random() * (i + 1));
+      const oldValue = cards[newIndex];
+      cards[newIndex] = cards[i];
+      cards[i] = oldValue;
+    }
+  }
+}
+
+class Card {
+  constructor(value, suit) {
+    this.value = value;
+    this.suit = suit;
+  }
+}
+
+function newDeck() {
+  return suits.flatMap((suit) => {
+    return values.map((value) => new Card(value, suit));
+  });
+}
